469170aaa15494f0b0f1ad5f2e1ba19fa7d6c811
kr1shnac <krishnacgowda10@gmail.com>
2026-08-25 20:58:32 +0530

    added tool and loop logic

--- agent.py
+++ agent.py
@@ -12,6 +12,61 @@ client = OpenAI(
 history = []
 
 
+#TOOL Definition
+TOOLS = [
+    {
+        "type": "function",
+        "function": {
+            "name": "write_file",
+            "description": "write the content to the file which user request",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "file_path": {
+                        "type": "string",
+                        "description": "The path where file exist and perform write operation"
+                    },
+                    "content":{
+                        "type": "string",
+                        "description": "The conetent which user requested and returned by AI as text will be written to file"
+                    }
+                }
+                "required": ["file_path", "content"]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "edit_file",
+            "description": "To edit the existing file content or makeing changes",
+            "properties": {
+                "type": "object",
+                "parameters" : {
+                    "file_path": {
+                        "type": "string",
+                        "description": "It is the path where the file is present and to navigate to the file edit. This will help."
+                    },
+                    "old_text": {
+                        "type": "string",
+                        "description": "This is where the old text is present in the content like which is already present before so that we can modify it further into new text"
+                    },
+                    "new_text": {
+                        "type": "string",
+                        "description": "This is where the modified like the content which need to be modified where we replace the old text or and add the new text for the file."
+                    }
+                },
+                "required": ["file_path", "old_text", "new_text"]
+            }
+        }
+    }
+]
+
+
+#TOOL EXECUTOR
+
+
+
 while True:
     try:
         user_input = input("You > ") 
@@ -33,11 +88,51 @@ while True:
             ] + history
         )
 
-        reply = response.choices[0].message.content
+        reply = response.choices[0].message
 
-        history.append({"role": "assistant", "content": reply})
+        if reply.tool_calls:
+            tool_calls_list = []
 
-        print("Retain > ", reply)
+            for tc in reply.tool_calls:
+                one_item = {
+                    "id": tc.id,
+                    "type": "function",
+                    "function": {
+                        "name": "tc.function.name",
+                        "arguments": "tc.function.arguments"
+                    }
+                }
+
+                tool_calls_list.append(one_item)
+
+            
+            history.append(
+                "role": "assistent",
+                "content": "reply.conetent",
+                "tool_calls": "tool_calls_list"
+            )
+
+        for tc in reply.tool_calls:
+            "tool_name": "tc.function.name",
+            "args": json.load("tc.function.arguments")
+
+            result = tool_dispatch["tool_name", "args"]
+
+        preview = result
+
+        print(preview)
+
+        history.append(
+            "role": "tool",
+            "tool_call_id": "id",
+            "content": result
+        )
+
+
+        else:
+            history.append({"role": "assistant", "content": reply})
+
+            print("Retain > ", reply)
 
     except Exception as e:
         print("Error:", e)
